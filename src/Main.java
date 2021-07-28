ab9db7cbb04b5db14ae968ea346a63e7552b0c4c
Victoria Cortes <vickydanyel.vc@gmail.com>
2021-07-28 19:14:39 -0400

    Update Interfaces and Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
+    public static void test() {
+        Registrar registrar = new Registrar();
+        Student s1 = new Student(registrar , "Student 1");
+        Student s2 = new Student(registrar, "Student 2");
+        Student s3 = new Student(registrar, "Student 3");
+        Student s4 = new Student(registrar, "Student 4");
+        Class c = new Class();
+
+        registrar.addListener(s1);
+        registrar.addListener(s2);
+        registrar.addListener(s3);
+        registrar.addListener(s4);
+
+
+
+        s1.addClass(c);
+        s2.addClass(c);
+        s3.addClass(c);
+        s4.addClass(c);
+
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        test();
+
     }
 }
--- src/Observable.java
+++ src/Observable.java
@@ -1,6 +1,6 @@
 public interface Observable {
     void addListener(Observer obs);
     void removeListener(Observer obs);
-    void notif(boolean available);
+    void notif(int seats);
 }
 
