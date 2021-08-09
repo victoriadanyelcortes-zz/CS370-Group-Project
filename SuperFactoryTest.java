5cbdbf3f6503e214e57b8efb036ebbb7c597ce24
Anthony <56104291+Zylem96@users.noreply.github.com>
2021-08-09 14:16:39 -0400

    Add files via upload

---
+++ SuperFactoryTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuperFactoryTest {
+
+    @org.junit.jupiter.api.Test
+    void printAllClasses() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printALLSubLists() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMathFactory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPhysicsFactory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCompSciFactory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getOtherFactory() {
+    }
+}
