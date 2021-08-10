655ef5a561e452c3eaf8c5fc3b632437144bda26
mrahari <72466962+mrahari@users.noreply.github.com>
2021-08-10 17:48:23 -0400

    Update WaitListIterator.java

--- WaitListIterator.java
+++ WaitListIterator.java
@@ -1,9 +1,11 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+// The WaitListIterator implements the Iterator interface which contains the methods next and hasNext
 public class WaitListIterator implements Iterator{
     private Queue<Student> WaitList = new LinkedList<Student>();
-
+    
+    // The WaitListIterator takes in a queue of students in the contructor  
     public WaitListIterator(Queue<Student> WaitList){
         this.WaitList = WaitList;
     }
