f12c0af3f536f05e17a3b96c42575f12bdab64ca
mrahari <72466962+mrahari@users.noreply.github.com>
2021-08-11 14:53:57 -0400

    Update Iterator.java

--- Iterator.java
+++ Iterator.java
@@ -1,4 +1,6 @@
 public interface Iterator {
+    // indicates if there are any more items to iterate over
     boolean hasNext();
+    // returns the next element in the collection 
     Object next();
 }
